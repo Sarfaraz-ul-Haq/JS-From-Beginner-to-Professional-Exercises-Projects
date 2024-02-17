138ef448ab75f19b26b44ed4650640f7e65516dc
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-17 11:49:01 +0500

    chapter project 1 - manipulating an array

---
+++ chap03_javascript_multiple_values/project1_manipulating_an_array.ts
@@ -0,0 +1,38 @@
+/* Take the following array:
+const theList = ['Laurence', 'Svekis', true, 35, null, undefined,
+{test: 'one', score: 55}, ['one', 'two']];
+
+Manipulate your array using various methods, such as pop(), push(), shift(), and
+unshift(), and transform it into the following:
+["FIRST", "Svekis", "MIDDLE", "hello World", "LAST"]
+
+You can take the following steps, or adopt your own approach:
+• Remove the first item and the last item.
+• Add FIRST to the start of the array.
+• Assign hello World to the fourth item value.
+• Assign MIDDLE to the third index value.
+• Add LAST to the last position in the array.
+• Output it to the console. */
+
+const theList = [
+  "Laurence",
+  "Svekis",
+  true,
+  35,
+  null,
+  undefined,
+  { test: "one", score: 55 },
+  ["one", "two"],
+];
+
+theList.shift();
+theList.pop();
+theList.unshift("FIRST");
+theList[2] = "MIDDLE";
+theList[3] = "hello World";
+theList.pop();
+theList.pop();
+theList.pop();
+theList.push("LAST");
+
+console.log(theList);
