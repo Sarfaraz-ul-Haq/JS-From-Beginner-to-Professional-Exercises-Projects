bcbba8d320e919bcae9d8bdf10ce199dad298515
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-20 17:41:00 +0500

    ex_4.3 completed

---
+++ chap04_logic_statements/ex_4.3.ts
@@ -0,0 +1,14 @@
+// 1. Create a Boolean value for an ID variable
+
+// 2. Using a ternary operator, create a message variable that will check whether
+// their ID is valid and either allow a person into a venue or not
+
+// 3. Output the response to the console
+
+let ID: boolean = true;
+
+let message = ID
+  ? "Your ID is valid and you're allowed to enter the venue."
+  : "Your ID is invalid and you're not allowed to enter the venue.";
+
+console.log(message);
